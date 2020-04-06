8156da073ac546494ed76c6acf4acf5145455cc6
Khai <Khai-Halim@users.noreply.github.com>
2020-04-06 23:47:11 +1000

    Add cancel functionality to search bar

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 //SEARCH BAR SHOW AND HIDE
 
 let searchIconI = document.querySelector('#search-icon-show-hide');
+let cancelIconI = document.querySelector('#cancel-search');
+let searchBlockDiv = document.querySelector('#search-block');
+let searchInput = document.querySelector('#search-input');
 
 searchIconI.addEventListener('click', function(){
-    let searchBlockDiv = document.querySelector('#search-block');
-    let searchInput = document.querySelector('#search-input');
-
     if(searchBlockDiv.style.height > "0px"){ 
+        searchInput.value = "";
         searchBlockDiv.style.height = "0px";
     }else{
         searchBlockDiv.style.height = "78px";
         searchInput.focus();
     }
+});
+
+//CANCEL SEARCH AND HIDE SEARCH BAR
+
+cancelIconI.addEventListener('click', function(){
+    searchInput.value = "";
+    searchBlockDiv.style.height = "0px";
 });
