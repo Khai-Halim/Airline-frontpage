d3d329271759faeec31b32686301b1fa44fc4fdf
Khai <Khai-Halim@users.noreply.github.com>
2020-04-06 23:41:06 +1000

    Added icons to header

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let searchIconI = document.querySelector('#search-icon');
-let searchBlockDiv = document.querySelector('#search-block');
-let searchInput = document.querySelector('#search-input');
+//SEARCH BAR SHOW AND HIDE
+
+let searchIconI = document.querySelector('#search-icon-show-hide');
 
 searchIconI.addEventListener('click', function(){
-    if(searchBlockDiv.style.height == "0px"){
-        
+    let searchBlockDiv = document.querySelector('#search-block');
+    let searchInput = document.querySelector('#search-input');
+
+    if(searchBlockDiv.style.height > "0px"){ 
+        searchBlockDiv.style.height = "0px";
+    }else{
         searchBlockDiv.style.height = "78px";
         searchInput.focus();
-    }else{
-        searchBlockDiv.style.height = "0px";
     }
 });
